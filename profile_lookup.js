c482744e05f8b912d6b06bbf66481c8591e8a72b
brianobbo <obbobrian1996@gmail.com>
2023-09-28 15:57:41 +0300

    made some changes in order to run the page on github pages

---
+++ profile_lookup.js
@@ -0,0 +1,55 @@
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    // Only change code below this line
+  
+    for (let i = 0; i < contacts.length; i++) {
+      if (name === contacts[i].firstName) {
+        if (contacts[i].hasOwnProperty(prop)) {
+            return contacts[i][prop];
+        } else {
+            return "No such property";
+            }
+        } 
+    }
+  
+    return "No such contact";
+    // Only change code above this line
+  }
+  
+  let lup = lookUpProfile("Akira", "likes");
+  console.log(lup);
+
+
+  var numArray = [];
+for (var i = 0; i < 3; i++) {
+  numArray.push(i);
+}
+console.log(numArray);
+console.log(i);
